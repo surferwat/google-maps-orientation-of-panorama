0f5ce2917bd46c2134a147e99068ca0451cf8c55
surferwat <wmiura@gmail.com>
2021-04-26 22:10:30 -0500

    fix: clean up

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ npm install
 
 Step 3: Build 
 ```
-npm run-script build:clean
+npm run-script build
 ```
 
 Step 4: Go to app folder and install the module
@@ -30,44 +30,44 @@ npm install /file/path/to/module
 
 ## Usage
 
-You may want to know whether a subject Street View panorama is to the left, in front of, or to the right of a map center point. `OrientationOfPanorama` provides a method, `computeOrientation`, that when called returns a `string` (LEFTOF, FRONTOF, or RIGHTOF). 
+You may want to know whether a subject Street View panorama is to the left, in front of, or to the right of a map center point. `OrientationOfPanorama` provides a method, `checkOrientation`, that when called returns a `string` (LEFTOF, FRONTOF, or RIGHTOF). 
 
-A subject Street View panorama could technically be on the left- or right-hand side, but optically could be considered in front of the map center. `OrientationOfPanorama` provides a method, `frontOfIsRange` which takes a boolean value to set the corresponding field. If set to `true`, a range of points would be used to represent the points in front of the map center point. If `false`, a single point would be used to represent the point in front of the map center. `frontOfIsRaneg` is set to `true` by default. The range is represented by the constant `DISTANCE_BETWEEN_BOUNDS`, which has a value of 5 (unit measure is in meters).
+A subject Street View panorama could technically be on the left- or right-hand side, but optically could be considered in front of the map center. `OrientationOfPanorama` provides a method, `frontOfIsRange` which takes a boolean value to set the corresponding field. If set to `true`, a range of points would be used to represent the points in front of the map center point. If `false`, a single point would be used to represent the point in front of the map center. `frontOfIsRange` is set to `true` by default. The range is represented by the constant `DISTANCE_BETWEEN_BOUNDS`, which has a value of 5 (unit measure is in meters).
 
 ```javascript
 import { Loader } from '@googlemaps/js-api-loader'
 import { OrientationOfPanorama } from 'google-maps-orientation-of-panorama'
 
+const frontOfPoint = {
+    lat: 1.277856235994971,
+    lng: 103.8481884743644
+}
+
+const mapCenterPoint = { 
+    lat: 1.2777384,
+    lng: 103.848394
+}
+
+const activePanoramaPoint = {
+    lat: 1.277778557217417,
+    lng: 103.8481407376543
+}
+
 const loader = new Loader({
   apiKey: 'YOUR_API_KEY',
   version: 'weekly',
-  ...additionalOptions,
 })
 
-let panoramaPov
 loader
     .load()
     .then(() => {
-        panoramaPov = new google.maps.StreetViewPanorama(
-            document.getElementById('street-view'),
-            {
-                position: { lat: 37.86926, lng: -122.254811 },
-                pov: { heading: 165, pitch: 0 },
-                zoom: 1
-            }
-        )
+        const orientationOfPanorama = new OrientationOfPanorama(frontOfPoint, mapCenterPoint)
+        orientationOfPanorama.activePanoramaPoint = activePanoramaPoint
+        orientationofPanorama
+            .checkOrientation()
+            .then((data) => console.log(data))
+            .catch((e) => console.error(e))
     })
-
-const mapCenterPoint = { 
-    lat: 37.86926, 
-    lng: -122.254811
-}
-
-const orientationOfPanorama = new OrientationOfPanorama(panoramaPov, mapCenterPoint)
-orientationOfPanorama
-    .computeOrientation()
-    .then((data) => console.log(data))
-    .catch((e) => console.error(e))
 ```
 
 ## Todo 
@@ -82,3 +82,4 @@ orientationOfPanorama
 * [Street View Containers](https://developers.google.com/maps/documentation/javascript/examples/streetview-embed#maps_streetview_embed-javascript)
 * [google.maps.LatLng vs google.maps.LatLngLiteral](https://stackoverflow.com/questions/54545979/google-maps-latlng-vs-google-maps-latlngliteral)
 * [Calculate if a point lies above or below (or right or left) of a line](https://math.stackexchange.com/questions/1435779/calculate-if-a-point-lies-above-or-below-or-right-to-left-of-a-line)
+* [Direction of a Point from a Line Segment](https://www.geeksforgeeks.org/direction-point-line-segment/)
--- src/index.ts
+++ src/index.ts
@@ -17,60 +17,25 @@ type FrontOfBounds = {
 }
 
 class OrientationOfPanorama {
-    private _panorama: google.maps.StreetViewPanorama // non-null assertion operator used
     private _mapCenterPoint: google.maps.LatLng
+    private _frontOfPoint: google.maps.LatLng
+    private _activePanoramaPoint: google.maps.LatLng
     private _frontOfIsRange: boolean = true
 
-    constructor(initPanorama: google.maps.StreetViewPanorama, initMapCenterPoint: google.maps.LatLng) {
-        this._panorama = initPanorama
+    constructor(
+        initFrontOfPoint: google.maps.LatLng,
+        initMapCenterPoint: google.maps.LatLng
+    ) {
+        this._frontOfPoint = initFrontOfPoint,
         this._mapCenterPoint = initMapCenterPoint
     }
 
-    set hasStretchedCenter(boolean: boolean) {
+    set isRange(boolean: boolean) {
         this._frontOfIsRange = boolean
     }
 
-    /**
-     * Calculates the angle A which represents the angle between the heading from the subject panorama to the 
-     * map center and the heading from the subject panorama to _front of_ point.
-     * @param heading1 
-     * @param heading2 
-     */
-    
-    private angleA(heading1: number, heading2: number) {
-        const diff = heading1 - heading2
-        let angle: number
-        if (Math.abs(diff) > 90 ) {
-            angle = Math.abs(diff - 180)
-        } else if (Math.abs(diff) < -90) {
-            angle = Math.abs(diff + 180)
-        } else {
-            angle = Math.abs(diff)
-        }
-        return angle
-    }
-
-    /**
-     * Calculates side A, which represents the distance from the map center to the _front of_ point (i.e., the point in 
-     * front of the map center that sits along the same path as the subject panorama).
-     * @param angleA 
-     * @param angleB 
-     * @param sideB 
-     */
-
-    private sideA(angleA: number, angleB: number, sideB: number): number {
-        return Math.sin(angleA) * (sideB / Math.sin(angleB))
-    }
-
-    /**
-     * Calculates angle C, which represents the angle between the heading from the map center to the subject panorama and 
-     * the heading from the map center to the _front of_ point.
-     * @param angleA 
-     * @param angleB 
-     */
-
-    private angleC(angleA: number, angleB: number): number {
-        return 180 - angleA - angleB
+    set activePanoramaPoint(newActivePanoramaPoint: google.maps.LatLng) {
+        this._activePanoramaPoint = newActivePanoramaPoint
     }
 
     /**
@@ -95,32 +60,31 @@ class OrientationOfPanorama {
      * @param bearings 
      */
 
-    private centerBounds(pointCenter: google.maps.LatLng, distanceToEnd: number, bearings: BearingsToFrontOfBounds): FrontOfBounds {
+    private frontOfBounds(frontOfPoint: google.maps.LatLng, distanceToEnd: number, bearings: BearingsToFrontOfBounds): FrontOfBounds {
         const bounds: FrontOfBounds = {
-            leftBound: google.maps.geometry.spherical.computeOffset(pointCenter, distanceToEnd, bearings.toLeftBound),
-            rightBound: google.maps.geometry.spherical.computeOffset(pointCenter, distanceToEnd, bearings.toRightBound)
+            leftBound: google.maps.geometry.spherical.computeOffset(frontOfPoint, distanceToEnd, bearings.toLeftBound),
+            rightBound: google.maps.geometry.spherical.computeOffset(frontOfPoint, distanceToEnd, bearings.toRightBound)
         }
         return bounds
     }
-    
+
     /**
-     * Calculates the y value (i.e., lat) that lies on the line that in this context represents the path from 
-     * the _center_ to the map.
-     * @param point1 
-     * @param xl 
+     * Calculates the dot product for points a, b, and p.
+     * @param a // The geo coordinates for the map center point
+     * @param b // The geo cooridinates for the default panorama of the map center point
+     * @param p // The geo coordinates for the active panorama
      */
 
-    private yValueOnLine(point1: google.maps.LatLng): number {
-        const y1: number = point1.lat()
-        const x1: number = point1.lng()
-        const y2: number = this._mapCenterPoint.lat()
-        const x2: number = this._mapCenterPoint.lng()
-        const xL: number = this._panorama.getPosition()!.lng()
+    private dotProduct(a: google.maps.LatLng, b: google.maps.LatLng, p: google.maps.LatLng) {
+        const ax: number = a.lat() 
+        const ay: number = a.lng()
+        const bx: number = b.lat() 
+        const by: number = b.lng() 
+        const px: number = p.lat() 
+        const py: number = p.lng() 
 
-        const m: number = (y2-y1) / (x2-x1)
-        const b: number = y2-(m*x2)
-        const yL: number = (m*xL)+b
-        return yL
+        const dp: number = (bx - ax) * (py - ay) - (by - ay) * (px - ax)
+        return dp
     }
 
     /**
@@ -128,72 +92,122 @@ class OrientationOfPanorama {
      * subject panorama point is to the left of, in front, or right of the map center point.
      */
 
-    computeOrientation(): PanoramaOrientation {
-        const panoramaPosition: google.maps.LatLng | null = this._panorama.getPosition()
-        if (panoramaPosition == null) {
-            throw new Error('could not get position of panorama')
-        }
+    checkOrientation(): PanoramaOrientation {
 
-        //// 1) We first need to compute the _front of_ point that is along the path of the panorama that is 
-        //// directly in front of the map. To do this, we figure out the angles and sides where we can 
-        //// and, subsequently, use the Law of Sines to calculate the side representing the distance from the 
-        //// map center point to the _front of_ point.
-
-        // Calculate angle A, which represents the angle between the heading from the subject panorama to the map center
-        // and the heading from subject panorama point to front of point 
-        const headingPanoramaMapCenter: number = google.maps.geometry.spherical.computeHeading(panoramaPosition, this._mapCenterPoint)
-        const headingPanoramaPhotographerPov: number = this._panorama.getPhotographerPov().heading
-        const angleA: number = this.angleA(headingPanoramaMapCenter, headingPanoramaPhotographerPov)
-
-        // Calculate side A, which represents the distance from the map center to the panorama center (i.e., the point in 
-        // front of the map center point that is along the path of the panorama)
-        const sideB: number = google.maps.geometry.spherical.computeDistanceBetween(panoramaPosition, this._mapCenterPoint) // distance from subject panorama to map center
-        const angleB: number = 90 // angle between the heading from front of panorama to the subject panorama and the heading from the front panorama to the map center
-        const sideA: number = this.sideA(angleA, angleB, sideB)
-
-        // Calculate angle C, which represents the angle between the heading from the map center to the subject panorama and the heading from the map center to the front of panorama
-        const angleC: number = this.angleC(angleA, angleB)
-
-        // Compute the _front of_ point
-        const pointCenter: google.maps.LatLng = google.maps.geometry.spherical.computeOffset(panoramaPosition, sideA, angleC)
-
-        //// 2) We next use the equation for a line in the 2D plane to solve for a y value that is on the line that
+        //// 1) We first use the equation for a line in the 2D plane to solve for a y value that is on the line that
         //// represents the path from the _front of_ to the map center. If withCenterBounds is set to true (which is so by 
         //// default), then _front of_ represents not just one point, but a range of points. So, we would have to calculate
         //// the y value for the left bound (i.e, left hand side or LHS) and the right bound (i.e., right hand side 
         //// or RHS).
 
-        let yLOnLhsLine: number 
-        let yLOnRhsLine: number
+        let dotProductLhs: number 
+        let dotProductRhs: number 
         if (this._frontOfIsRange) {
-            const headingFrontOfMapCenter: number = google.maps.geometry.spherical.computeHeading(pointCenter, this._mapCenterPoint)
+            const headingFrontOfMapCenter: number = google.maps.geometry.spherical.computeHeading(this._frontOfPoint, this._mapCenterPoint)
             const bearingsFrontOfBounds: BearingsToFrontOfBounds = this.bearingsTowardsFrontOfBounds(headingFrontOfMapCenter)
-            const pointsFrontOfBounds: FrontOfBounds = this.centerBounds(pointCenter, DISTANCE_BETWEEN_BOUNDS/2, bearingsFrontOfBounds)
-            
-            yLOnLhsLine = this.yValueOnLine(pointsFrontOfBounds.leftBound)
-            yLOnRhsLine = this.yValueOnLine(pointsFrontOfBounds.rightBound)
+            const frontOfBoundsPoints: FrontOfBounds = this.frontOfBounds(this._frontOfPoint, DISTANCE_BETWEEN_BOUNDS/2, bearingsFrontOfBounds)
+
+            dotProductLhs = this.dotProduct(this._mapCenterPoint, frontOfBoundsPoints.leftBound, this._activePanoramaPoint)
+            dotProductRhs = this.dotProduct(this._mapCenterPoint, frontOfBoundsPoints.rightBound, this._activePanoramaPoint)
         } else {
-            const ylOnLine : number = this.yValueOnLine(pointCenter)
-            yLOnLhsLine = ylOnLine
-            yLOnRhsLine = ylOnLine
+            dotProductLhs = this.dotProduct(this._mapCenterPoint, this._frontOfPoint, this._activePanoramaPoint)
+            dotProductRhs = this.dotProduct(this._mapCenterPoint, this._frontOfPoint, this._activePanoramaPoint)
         }
 
-        //// 3) We finally can determine whether the subject panorama facing the map center is doing so from the left 
+        //// 2) We finally can determine whether the subject panorama facing the map center is doing so from the left 
         //// hand side, center, or right hand side by checking the difference between the y value that lies on the line that 
         //// represents the path from the _front of_ to the map center and the y value of the panorama.
-        
+
         let orientation: PanoramaOrientation
-        const yP = this._panorama.getPosition()!.lat()
-        if (yLOnLhsLine - yP < 0) {
-            orientation = PanoramaOrientation.LeftOf
-        } else if (yLOnRhsLine - yP > 0) {
+        if (dotProductLhs < 0) {
+            // Left of, from perspective of map center
+            // So, right of, from perspective of panorama
             orientation = PanoramaOrientation.RightOf
+        } else if (dotProductRhs > 0) {
+            // Right of, from perspective of map center
+            // So, left of, from perspective of panorama
+            orientation = PanoramaOrientation.LeftOf
         } else {
             orientation = PanoramaOrientation.FrontOf
         }
-
         return orientation
     }
 }
 
 export { OrientationOfPanorama }
+
+// // Calculates the point in front of the map center. However, we assume that 
+// // the default panorama is in front of the map center, so we do not use this 
+// // function at the moment.
+//
+// private _defaultPanoramaPhotographerPovHeading: number // Be sure to initialize in constructor
+// private _defaultPanoramaPoint: google.maps.LatLng // Be sure to initialize in constructor
+//
+// /**
+//  * Calculates the angle A which represents the angle between the heading from the subject panorama to the 
+//  * map center and the heading from the subject panorama to _front of_ point.
+//  * @param heading1 
+//  * @param heading2 
+//  */
+
+// private angleA(heading1: number, heading2: number) {
+//     const diff = heading1 - heading2
+//     let angle: number
+//     if (Math.abs(diff) > 90 ) {
+//         angle = Math.abs(diff - 180)
+//     } else if (Math.abs(diff) < -90) {
+//         angle = Math.abs(diff + 180)
+//     } else {
+//         angle = Math.abs(diff)
+//     }
+//     return angle
+// }
+
+// /**
+//  * Calculates side A, which represents the distance from the map center to the _front of_ point (i.e., the point in 
+//  * front of the map center that sits along the same path as the subject panorama).
+//  * @param angleA 
+//  * @param angleB 
+//  * @param sideB 
+//  */
+
+// private sideA(angleA: number, angleB: number, sideB: number): number {
+//     return Math.sin(angleA) * (sideB / Math.sin(angleB))
+// }
+
+// /**
+//  * Calculates angle C, which represents the angle between the heading from the map center to the subject panorama and 
+//  * the heading from the map center to the _front of_ point.
+//  * @param angleA 
+//  * @param angleB 
+//  */
+
+// private angleC(angleA: number, angleB: number): number {
+//     return 180 - angleA - angleB
+// }
+
+// /**
+//  * We need to instatiate the _front of_ point that is along the path of the panorama that is 
+//  * directly in front of the map. To do this, we figure out the angles and sides where we can 
+//  * and, subsequently, use the Law of Sines to calculate the side representing the distance from the 
+//  * map center point to the _front of_ point.
+//  */
+
+// frontOfPoint() {
+//     // Calculate angle A, which represents the angle between the heading from the subject panorama to the map center
+//     // and the heading from subject panorama point to front of point 
+//     const headingPanoramaMapCenter: number = google.maps.geometry.spherical.computeHeading(this._defaultPanoramaPoint, this._mapCenterPoint)
+//     const angleA: number = this.angleA(headingPanoramaMapCenter, this._defaultPanoramaPhotographerPovHeading)
+
+//     // Calculate side A, which represents the distance from the map center to the panorama center (i.e., the point in 
+//     // front of the map center point that is along the path of the panorama)
+//     const sideB: number = google.maps.geometry.spherical.computeDistanceBetween(this._defaultPanoramaPoint, this._mapCenterPoint) // distance from subject panorama to map center
+//     const angleB: number = 90 // angle between the heading from front of panorama to the subject panorama and the heading from the front panorama to the map center
+//     const sideA: number = this.sideA(angleA, angleB, sideB)
+
+//     // Calculate angle C, which represents the angle between the heading from the map center to the subject panorama and the heading from the map center to the front of panorama
+//     const angleC: number = this.angleC(angleA, angleB)
+
+//     // Compute the _front of_ point
+//     this._frontOfPoint = google.maps.geometry.spherical.computeOffset(this._defaultPanoramaPoint, sideA, angleC)
+// }
